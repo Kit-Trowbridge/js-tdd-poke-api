aa2f87055970dee92cd5f10e92d7507d0f6ec0a1
kit-trowbridge <kittrow.dev@gmail.com>
2026-05-28 12:10:26 +0100

    test function set up

---
+++ fetchPokemonData.test.js
@@ -0,0 +1,7 @@
+
+describe("fetchPokemonData", () => {
+    it("returns the name and sprites for an existing Pokemon", async () => {
+        const pokemonData = await fetchPokemonData("ditto");
+        expect(pokemonData).toEqual({name: "ditto", sprites: { front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" }});
+    })
+})
