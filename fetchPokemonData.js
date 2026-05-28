637dc671ee5c58b9d744f94db77328629d5919d2
kit-trowbridge <kittrow.dev@gmail.com>
2026-05-28 12:17:59 +0100

    test passing for happy path

--- fetchPokemonData.js
+++ fetchPokemonData.js
@@ -1,5 +1,9 @@
-const fetchPokemonData = () => {
-
+const fetchPokemonData = async (pokemon) => {
+    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const rawPokemonData = await request.json();
+    const pokemonData = {name: rawPokemonData.species.name, sprites: {front_default: rawPokemonData.sprites.front_default}};
+    console.log(pokemonData);
+    return pokemonData;
 }
 
 module.exports = fetchPokemonData;
