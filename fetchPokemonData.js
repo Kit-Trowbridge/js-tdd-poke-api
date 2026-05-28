67098e693e66e729c1391572b1eb92da0023c812
kit-trowbridge <kittrow.dev@gmail.com>
2026-05-28 12:27:22 +0100

    error handling for non string test passing

--- fetchPokemonData.js
+++ fetchPokemonData.js
@@ -1,9 +1,16 @@
 const fetchPokemonData = async (pokemon) => {
-    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const rawPokemonData = await request.json();
-    const pokemonData = {name: rawPokemonData.species.name, sprites: {front_default: rawPokemonData.sprites.front_default}};
-    console.log(pokemonData);
-    return pokemonData;
+    if (typeof(pokemon) !== "string") {
+        throw new Error("Pokemon must be in string format")
+    }
+    try {
+        const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        const rawPokemonData = await request.json();
+        const pokemonData = {name: rawPokemonData.species.name, sprites: {front_default: rawPokemonData.sprites.front_default}};
+        return pokemonData;
+    } catch(error) {
+        throw error; 
+    }
+
 }
 
 module.exports = fetchPokemonData;
--- fetchPokemonData.test.js
+++ fetchPokemonData.test.js
@@ -4,5 +4,8 @@ describe("fetchPokemonData", () => {
     it("returns the name and sprites for an existing Pokemon", async () => {
         const pokemonData = await fetchPokemonData("ditto");
         expect(pokemonData).toEqual({name: "ditto", sprites: { front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" }});
+    }) 
+    it("throws an error when the Pokemon is not a string", async () => {
+        await expect(fetchPokemonData(1)).rejects.toThrow("Pokemon must be in string format");
     })
 })
